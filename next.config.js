af19b9f77eca789f32411f7965cb6cfc141d3b64
Sumeshpad <sumeshpad1994@gmail.com>
2025-05-25 23:49:26 +0530

    4

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // remove export
   reactStrictMode: true,
   experimental: {
     appDir: true
   }
-}
+};
 
 module.exports = nextConfig;
